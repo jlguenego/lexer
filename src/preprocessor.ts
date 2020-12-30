9b9027fe7d744c45309d99f0e39a6f1cd0ac299b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-30 19:14:14 +0100

    refactor: state

--- src/preprocessor.ts
+++ src/preprocessor.ts
@@ -1,15 +1,12 @@
-import {TokenElement} from './interfaces/State';
+import {State} from './interfaces/State';
 import {applyTokenOnSourceElement} from './misc';
 import {SourceElement} from './SourceElement';
 import {Rule} from './Rule';
 import {Token} from './Token';
 
-export const preprocess = (
-  initialState: TokenElement[],
-  tokens: Rule[]
-): TokenElement[] => {
-  let previousState: TokenElement[] = [];
-  let state: TokenElement[] = initialState;
+export const preprocess = (initialState: State, tokens: Rule[]): State => {
+  let previousState: State = [];
+  let state: State = initialState;
   while (state !== previousState) {
     previousState = state;
     state = getNextState(previousState, tokens);
@@ -18,11 +15,11 @@ export const preprocess = (
   return state;
 };
 
-const getLength = (r: TokenElement[]): number => {
+const getLength = (r: State): number => {
   return r[0] instanceof Token ? 0 : r[0].text.length;
 };
 
-export const getNextState = (state: TokenElement[], tokens: Rule[]) => {
+export const getNextState = (state: State, tokens: Rule[]) => {
   const sourceElt = state[state.length - 1];
   if (!(sourceElt instanceof SourceElement)) {
     return state;
@@ -37,7 +34,7 @@ export const getNextState = (state: TokenElement[], tokens: Rule[]) => {
         const accLength = getLength(acc);
         return rLength < accLength ? r : acc;
       },
-      [sourceElt] as TokenElement[]
+      [sourceElt] as State
     );
   if (array[0] === sourceElt) {
     return state;
