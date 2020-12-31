05018a200bd3de9a42fb7218b652f5b1111dd859
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-31 13:16:52 +0100

    refactor: position variable name

--- src/tools/apply-rule.ts
+++ src/tools/apply-rule.ts
@@ -25,34 +25,35 @@ export const applyRuleOnSourceElement = (
   if (!matched) {
     return [elt];
   }
-  const result: State = [];
+  const state: State = [];
   if (matched.index === undefined) {
     throw new Error(
       'matched exists and index not present. Case not implemented.'
     );
   }
   const prefix = elt.text.substr(0, matched.index);
-  const posStart = elt.position;
-  const posMatch = positionAdd(posStart, prefix);
-  const posSuffix = positionAdd(posMatch, matched[0]);
+  const startPos = elt.position;
+  const matchPos = positionAdd(startPos, prefix);
+  const suffixPos = positionAdd(matchPos, matched[0]);
   if (matched.index > 0) {
-    result.push(new SourceElement(prefix, posStart));
+    state.push(new SourceElement(prefix, startPos));
   }
   if (!rule.ignore) {
-    result.push({
+    // add the token in the state
+    state.push({
       name: rule.name,
       value: matched[0],
       group: rule.group,
-      position: posMatch,
+      position: matchPos,
     });
   }
-  const remainingIndex = matched.index + matched[0].length;
+  const suffixIndex = matched.index + matched[0].length;
 
-  if (remainingIndex < elt.text.length) {
-    const se = new SourceElement(elt.text.substr(remainingIndex), posSuffix);
+  if (suffixIndex < elt.text.length) {
+    const se = new SourceElement(elt.text.substr(suffixIndex), suffixPos);
     const array = matchAll ? applyRuleOnSourceElement(se, rule) : [se];
-    result.push(...array);
+    state.push(...array);
   }
 
-  return result;
+  return state;
 };
